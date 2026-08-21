0020696b943305dc0ccb200f38ca1d239e30b6dd
Navya Bhatia <navyabhatia48@gmail.com>
2026-08-21 13:34:29 +0530

    Add toast notifications with sonner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,8 @@ import Pipeline from "./pages/Pipeline";
 import Dashboard from "./pages/Dashboard";
 import Analytics from "./pages/Analytics";
 import Reminders from "./pages/Reminders";
+import { Toaster } from "sonner";
+import AppLayout from "./components/layout/AppLayout";
 
 function App() {
   return (
@@ -17,56 +19,28 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
           <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/applications"
-            element={
-              <ProtectedRoute>
-                <Applications />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-          path="/applications/:id"
           element={
           <ProtectedRoute>
-            <ApplicationDetail />
+            <AppLayout />
           </ProtectedRoute>
           }
-          />
+          >
+            
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/applications" element={<Applications />} />
+            <Route path="/applications/:id" element={<ApplicationDetail />} />
+            <Route path="/pipeline" element={<Pipeline />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/reminders" element={<Reminders />} />
+
+          </Route>
+
           <Route path="/" element={<Login />} />
-          <Route
-          path="/pipeline"
-          element={
-          <ProtectedRoute>
-            <Pipeline />
-            </ProtectedRoute>
-          }
-          />
-          <Route
-          path="/analytics"
-          element={
-          <ProtectedRoute>
-            <Analytics />
-          </ProtectedRoute>
-          }
-          />
-          <Route
-          path="/reminders"
-          element={
-          <ProtectedRoute>
-            <Reminders />
-          </ProtectedRoute>
-          }
-          />
         </Routes>
+
+        <Toaster position="top-right" richColors closeButton />
       </AuthProvider>
     </BrowserRouter>
   );
